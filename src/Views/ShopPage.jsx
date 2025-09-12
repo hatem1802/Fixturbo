23555d507fd39729129b9c5019f97308a2430feb
hatem1802 <125581921+hatem1802@users.noreply.github.com>
2025-09-12 16:13:33 +0300

    Update ShopPage.jsx

--- src/Views/ShopPage.jsx
+++ src/Views/ShopPage.jsx
@@ -165,7 +165,7 @@ const Products = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("https://localhost:3000/api/students")
+    fetch("http://localhost:3000/api/students/")
       .then((Response) => {
         return Response.json();
       })
